d6caa6b8560d3f4516de05f045dcbc8f6f57d205
Manoj828 <manojanand828@gmail.com>
2021-11-02 06:51:33 -0700

    updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
 
+// connecting to mongodb
 connectDB();
 
 const app = express();
